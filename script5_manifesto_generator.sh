2b4672949b1324002b512e13d142cb7ea7bad1a1
bhalu-git169 <bhalendushrivastava90@gmail.com>
2026-03-30 22:37:02 +0530

    added comments and improved script readability

--- script5_manifesto_generator.sh
+++ script5_manifesto_generator.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # ============================================================
 # Script 5: Open Source Manifesto Generator
-# Author: BHALENDU SHRIVASTAVA| Reg No: 24MIP10169
+# Author: BHALENDU SHRIVASTAVA| Reg No: 24MIP10169 
 # Course: Open Source Software | OSS NGMC Capstone Project
 # Description: Interactively asks the user three questions
 #              and composes a personalised open-source
